efed6dd157374ca43cb72f9f4831770628d9563a
im_loki <lokeshwar1998@gmail.com>
2020-06-03 12:27:24 +0530

    CSV Update

--- Scheduler_FYP/Dictionary_FYP/dict_using_spellchecker.py
+++ Scheduler_FYP/Dictionary_FYP/dict_using_spellchecker.py
@@ -2,7 +2,7 @@ from spellchecker import SpellChecker
 
 def spell_check_me(sentence):
 	spell = SpellChecker()
-	spell.word_frequency.load_words(['microsoft', 'mp3','apple', 'google','hadoop','json','samsung', 'backend', 'ocr', 'keyphrase', 'MRI', 'turn-up','nauseam', 'vast', 'turn', 'NuVox'])
+	spell.word_frequency.load_words(['microsoft', 'mp3', 'broadband','apple', 'google','hadoop','json','samsung', 'backend', 'ocr', 'keyphrase', 'MRI', 'turn-up','nauseam', 'vast', 'turn', 'NuVox'])
 	temp = ""
 
 	for word in sentence.split(" "):
